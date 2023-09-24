2895628cb464ad278e1b57925e1aabce4521bf5a
MinhTam <lait2553@gmail.com>
2023-09-24 14:43:16 +0700

    feat: Added index.jsp and set up navigation in ServletController

--- week02_client/src/main/java/vn/edu/iuh/fit/week02_client/controllers/ServletController.java
+++ week02_client/src/main/java/vn/edu/iuh/fit/week02_client/controllers/ServletController.java
@@ -12,8 +12,19 @@ import java.io.IOException;
 public class ServletController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
+        String action = req.getParameter("action");
+        switch (action) {
+            case "products":
+                break;
+            case "cart":
+                break;
+            case "checkout":
+                break;
+            default:
+                break;
+        }
     }
+
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
